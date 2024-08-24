30dfe7cfb4c39f830e5c424b52c60da424d39c83
Kozman Botond <kozmanbotond1@gmail.com>
2024-08-24 17:21:33 +0200

    Tracker added to application

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# tracker_application
+# Tracker application
+
+# Requirements
+The Beam application is installed on your system;
+you have an active subscription, which makes you able to receive head and eye tracking data;
+you run the calibration procedure within the Beam application at least once.
---
+++ application.py
@@ -0,0 +1,30 @@
+import API.python.tracker_sample as tracker_sample  # Now you can import it
+
+tracker = tracker_sample.TrackerClient()
+
+def initialize_tracker():
+    tracker.initialize()
+
+def start_tracker():
+    try:
+        tracker.start()
+        print("Tracking started successfully")
+    except Exception as e:
+        print(f"Failed to start tracking: {e}")
+
+def tracking_cursor_position():
+    tracking_data = tracker.get_tracking_data()
+    gaze_x = tracking_data.get("gaze_x")
+    gaze_y = tracking_data.get("gaze_y")
+    print(f"Gaze coordinates: X={gaze_x}, Y={gaze_y}")
+
+def stop_tracker():
+    tracker.stop()
+    print("Tracking stopped")
+
+if __name__ == "__main__":
+    initialize_tracker()
+    start_tracker()
+    if (tracker.is_running()):
+        tracking_cursor_position()
+        stop_tracker()

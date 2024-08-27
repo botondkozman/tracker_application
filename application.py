9b9282fbca0b8a1c2ca0723ed207d7b584259054
Kozman Botond <kozmanbotond1@gmail.com>
2024-08-27 20:47:32 +0200

    Started to implement async running, but does not work

--- application.py
+++ application.py
@@ -4,27 +4,28 @@ import glob
 import os
 from PIL import Image, ImageTk
 import tkinter as tk
-import numpy as np
+import asyncio
+import threading
 
 images_path = "../../../Downloads/parquet_images"
 images = None
 tracker = None
+is_tracking_needed = False
 
 def initialize_tracker():
     global tracker
     tracker = TrackerClient()
-    print(tracker.connected)
-    print(tracker)
 
 def connect_tracker():
-    while (tracker.connected == False) :
-        MESSAGE_PERIOD_IN_SECONDS = 2
+    while not tracker.connected :
+        MESSAGE_PERIOD_IN_SECONDS = 1
         time.sleep(MESSAGE_PERIOD_IN_SECONDS - time.monotonic() % MESSAGE_PERIOD_IN_SECONDS)
         print("No connection with tracker server")
 
-def tracking_cursor_position():
-    screen_gaze = tracker.get_screen_gaze_info()
-    print("Coordinates: <x=%5.3f px, y=%5.3f px>" % (screen_gaze.x, screen_gaze.y))
+async def tracking_cursor_position():
+     while is_tracking_needed:
+        screen_gaze = tracker.get_screen_gaze_info()
+        print("Coordinates: <x=%5.3f px, y=%5.3f px>" % (screen_gaze.x, screen_gaze.y))
 
 def parse_folder():
     global images
@@ -33,7 +34,7 @@ def parse_folder():
     if not images:
         print("The folder does not contains images")
 
-def show_image_with_input(image_path):
+async def show_image(image_path):
     root = tk.Tk()
     root.title("Images")
 
@@ -57,6 +58,7 @@ def show_image_with_input(image_path):
     number_entry.focus_force()
 
     def on_submit(event):
+        global is_tracking_needed
         if event.char.isdigit():
             user_input = number_entry.get()
             number_entry.delete(0, tk.END)
@@ -64,21 +66,49 @@ def show_image_with_input(image_path):
             number = int(user_input)
             print(f"Rating: {number}")
             root.destroy()
+            is_tracking_needed = False
 
     root.bind('<Key>', on_submit)
-    root.mainloop()
+    
+    def run_tkinter_loop():
+        root.mainloop()
 
-if __name__ == "__main__":
+    # Run Tkinter event loop in a separate thread
+    tkinter_thread = threading.Thread(target=run_tkinter_loop)
+    tkinter_thread.start()
+
+    # Wait for the Tkinter thread to finish
+    tkinter_thread.join()
+
+async def process_image(image_path) :
+    global is_tracking_needed
+    start_time = time.time()
+    is_tracking_needed = True
+    tracking_task = asyncio.ensure_future(tracking_cursor_position())
+    show_image_taks = asyncio.ensure_future(show_image(image_path))
+    done, pending = await asyncio.wait([show_image_taks, tracking_task], return_when=asyncio.FIRST_COMPLETED)
+    for task in pending:
+        print(f"Canceling {task.get_name()}")
+        task.cancel()
+
+    for task in done:
+        await task
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time} seconds")
+
+async def main():
     initialize_tracker()
     parse_folder()
     connect_tracker()
     if tracker.connected:
         for image in images:
-            start_time = time.time()
-            tracking_cursor_position()
-            show_image_with_input(image)
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            print(f"Elapsed time: {elapsed_time} seconds")
+            await process_image(image)
     else:
         print("No connection with tracker server")
+
+if __name__ == "__main__":
+    print("Running")
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()

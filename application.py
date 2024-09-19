70c770677e43261e40aa87019c68aea771c878e6
Kozman Botond <kozmanbotond1@gmail.com>
2024-09-19 23:29:37 +0200

    Syncron running is working with threading usage

--- application.py
+++ application.py
@@ -10,22 +10,24 @@ import threading
 images_path = "../../../Downloads/parquet_images"
 images = None
 tracker = None
-is_tracking_needed = False
+close_event = None
 
 def initialize_tracker():
     global tracker
     tracker = TrackerClient()
 
 def connect_tracker():
-    while not tracker.connected :
+    while not tracker.connected:
         MESSAGE_PERIOD_IN_SECONDS = 1
         time.sleep(MESSAGE_PERIOD_IN_SECONDS - time.monotonic() % MESSAGE_PERIOD_IN_SECONDS)
         print("No connection with tracker server")
 
 async def tracking_cursor_position():
-     while is_tracking_needed:
+    global close_event
+    while not close_event.is_set():
         screen_gaze = tracker.get_screen_gaze_info()
         print("Coordinates: <x=%5.3f px, y=%5.3f px>" % (screen_gaze.x, screen_gaze.y))
+        await asyncio.sleep(0.1)
 
 def parse_folder():
     global images
@@ -34,7 +36,7 @@ def parse_folder():
     if not images:
         print("The folder does not contains images")
 
-async def show_image(image_path):
+def show_image_tk(image_path, close_event):
     root = tk.Tk()
     root.title("Images")
 
@@ -49,7 +51,7 @@ async def show_image(image_path):
     label = tk.Label(root, image = img_tk)
     label.pack()
 
-    number_label = tk.Label(root, text="Give a rate:")
+    number_label = tk.Label(root, text = "Give a rate:")
     number_label.pack()
 
     number_entry = tk.Entry(root)
@@ -58,41 +60,46 @@ async def show_image(image_path):
     number_entry.focus_force()
 
     def on_submit(event):
-        global is_tracking_needed
         if event.char.isdigit():
             user_input = number_entry.get()
             number_entry.delete(0, tk.END)
             number_entry.insert(0, user_input)
             number = int(user_input)
             print(f"Rating: {number}")
-            root.destroy()
-            is_tracking_needed = False
+            on_close()
 
     root.bind('<Key>', on_submit)
-    
-    def run_tkinter_loop():
-        root.mainloop()
 
-    # Run Tkinter event loop in a separate thread
-    tkinter_thread = threading.Thread(target=run_tkinter_loop)
-    tkinter_thread.start()
+    def on_close():
+        print("closing")
+        close_event.set()
+        root.destroy()
 
-    # Wait for the Tkinter thread to finish
-    tkinter_thread.join()
+    root.protocol("WM_DELETE_WINDOW", on_close)
+    root.mainloop()
 
-async def process_image(image_path) :
-    global is_tracking_needed
+async def show_image(image_path):
+    global close_event
+    close_event = threading.Event()
+
+    tk_thread = threading.Thread(target=show_image_tk, args=(image_path, close_event))
+    tk_thread.start()
+
+    while not close_event.is_set():
+        await asyncio.sleep(0.1)
+
+    tk_thread.join()
+
+async def process_image(image_path):
+    global show_image_task
     start_time = time.time()
-    is_tracking_needed = True
-    tracking_task = asyncio.ensure_future(tracking_cursor_position())
-    show_image_taks = asyncio.ensure_future(show_image(image_path))
-    done, pending = await asyncio.wait([show_image_taks, tracking_task], return_when=asyncio.FIRST_COMPLETED)
-    for task in pending:
-        print(f"Canceling {task.get_name()}")
-        task.cancel()
 
-    for task in done:
-        await task
+    show_image_task = asyncio.ensure_future(show_image(image_path))
+    tracking_task = asyncio.ensure_future(tracking_cursor_position())
+
+    await show_image_task
+    await tracking_task
+
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Elapsed time: {elapsed_time} seconds")
